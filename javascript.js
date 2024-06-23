beaf3e567f4388b6ba6114cda2405e06523dc4da
ZephyRemix <hunyaochong@gmail.com>
2024-06-23 18:06:45 +0800

    Add functionality to button to redraw grid based on desired row and col number

--- javascript.js
+++ javascript.js
@@ -1,24 +1,47 @@
-const GRID_ROW = 10;
-const GRID_COL = 10;
+let rowNum = 20;
+let colNum = 20;
 
 let gridMap = document.querySelector(".gridMap");
 let gridSizeToggle = document.querySelector("button");
 
-// create 16x16 grid of square divs
-for (let row = 0; row < GRID_ROW; row++) {
-    let gridRow = document.createElement("div");
-    gridRow.setAttribute("class", "row");
-    
-    for (let col = 0; col < GRID_COL; col++) {
-        let gridCol = document.createElement("div");
-        gridCol.setAttribute("class", "box");
-        gridCol.addEventListener('mouseover', (event) => changeColor(event.target));
-        gridRow.appendChild(gridCol);
+gridSizeToggle.addEventListener('click', () => {
+    let desiredGridNum = Number(prompt("Enter number of rows and columns", "Max of 100"));
+    if (desiredGridNum > 100) {
+        alert("Grid size cannot be larger than 100!");
+    } else {
+        rowNum = desiredGridNum;
+        colNum = desiredGridNum;
+        removeAllChildNodes(gridMap);
+        drawGrids();
     }
-    gridMap.appendChild(gridRow);
+});
+
+// create 16x16 grid of square divs
+function drawGrids () {
+    for (let row = 0; row < rowNum; row++) {
+        let gridRow = document.createElement("div");
+        gridRow.setAttribute("class", "row");
+        
+        for (let col = 0; col < colNum; col++) {
+            let gridCol = document.createElement("div");
+            gridCol.setAttribute("class", "box");
+            gridCol.addEventListener('mouseover', (event) => changeColor(event.target));
+            gridRow.appendChild(gridCol);
+        }
+        gridMap.appendChild(gridRow);
+    }
 }
 
 function changeColor(box) {
     console.log(box);
     box.setAttribute("class", "color");
 }
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        console.log("removing")
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+drawGrids();
